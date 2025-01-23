b6b6b1867a639fea4702b5fa16f77d842087e276
snowFormacao <constantinosobredotado@gmail.com>
2025-01-23 15:27:43 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,5 @@ if ingredients_list:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_dt = st.dataframe(data= smoothiefroot_response.json(), use_container_witdh= True)
+st.text(smoothiefroot_response)
         
